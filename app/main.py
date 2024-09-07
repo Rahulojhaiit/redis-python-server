9a15d7aa5487fcf3a661ab13573e8b133e889b74
Rahul K.Ojha <rahulojhaiit@gmail.com>
2024-09-07 13:27:51 +0530

    Read more message

--- app/main.py
+++ app/main.py
@@ -8,21 +8,23 @@ def main():
     # Uncomment this to pass the first stage
     
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.listen(1)
+    server_socket.listen(2)
 
     client_socket, client_address = server_socket.accept() # wait for client
     print(f"Connection established wih {client_address}")
 
-    data = client_socket.recv(1024)
+    data = client_socket.recv(2048)
     print(f"Received data: `{data.decode()}`")
 
-    resp = data
-    if data == b'*1\r\n$4\r\nPING\r\n':
-        resp = b'+PONG\r\n'
+    resp =  b'+PONG\r\n'
+    client_socket.send(resp)
+    data = client_socket.recv(2048)
+    if data: 
+        print(f"Received data: `{data.decode()}`")
+        resp =  b'+PONG\r\n'
+        client_socket.send(resp)
 
-    client_socket.sendall(resp)
     client_socket.close()
-
 
 
 if __name__ == "__main__":

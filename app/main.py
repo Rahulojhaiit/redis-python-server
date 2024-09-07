6445d2809efe4811dc7fc84a6356c82ae38cc753
Rahul K.Ojha <rahulojhaiit@gmail.com>
2024-09-07 11:47:48 +0530

    PING Response

--- app/main.py
+++ app/main.py
@@ -8,7 +8,21 @@ def main():
     # Uncomment this to pass the first stage
     
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    server_socket.listen(1)
+
+    client_socket, client_address = server_socket.accept() # wait for client
+    print(f"Connection established wih {client_address}")
+
+    data = client_socket.recv(1024)
+    print(f"Received data: `{data.decode()}`")
+
+    resp = data
+    if data == b'*1\r\n$4\r\nPING\r\n':
+        resp = b'+PONG\r\n'
+
+    client_socket.sendall(resp)
+    client_socket.close()
+
 
 
 if __name__ == "__main__":
